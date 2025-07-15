411bef8ac60c7c7773f3ca7432d6c60a2a417100
SanskarYerawar <saapkstore@gmail.com>
2025-07-15 20:57:55 +0530

    Create greet.py

---
+++ modules/greet/greet.py
@@ -0,0 +1,8 @@
+def hi(name):
+    return f"Hi {name}, how are you?"
+
+def bye(name):
+    return f"Goodbye {name}, see you soon!"
+
+def greet_formal(name):
+    return f"Hello Mr./Ms. {name}, hope you're doing well!"
